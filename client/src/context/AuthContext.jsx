b17ea79c0da0a3e9a9c82443b8860e9b6f9e42ed
Paul Gomis <gomispapaul6@gmail.com>
2024-05-09 12:44:45 +0000

    Create Chats

--- client/src/context/AuthContext.jsx
+++ client/src/context/AuthContext.jsx
@@ -20,14 +20,11 @@ export const AuthContextProvider = ({ children }) => {
     password: "",
   });
 
-  console.log(user)
-  console.log({loginInfo})
-
   useEffect(() => {
     const user = localStorage.getItem("User");
 
     setUser(JSON.parse(user));
-  }, [])
+  }, []);
 
   const updateRegisterInfo = useCallback((info) => {
     setRegisterInfo(info);
@@ -37,50 +34,56 @@ export const AuthContextProvider = ({ children }) => {
     setLoginInfo(info);
   }, []);
 
-  const registerUser = useCallback(async (e) => {
-    e.preventDefault();
-    setIsRegisterLoading(true);
-    setRegisterError(null);
+  const registerUser = useCallback(
+    async (e) => {
+      e.preventDefault();
+      setIsRegisterLoading(true);
+      setRegisterError(null);
 
-    const response = await postRequest(
-      `${baseUrl}/users/register`,
-      JSON.stringify(registerInfo)
-    );
+      const response = await postRequest(
+        `${baseUrl}/users/register`,
+        JSON.stringify(registerInfo)
+      );
 
-    setIsRegisterLoading(false);
+      setIsRegisterLoading(false);
 
-    if (response.error) {
-      return setRegisterError(response);
-    }
+      if (response.error) {
+        return setRegisterError(response);
+      }
 
-    localStorage.setItem("User", JSON.stringify(response));
-    setUser(response);
-  }, [registerInfo]);
+      localStorage.setItem("User", JSON.stringify(response));
+      setUser(response);
+    },
+    [registerInfo]
+  );
 
-  const loginUser = useCallback(async(e) => {
-    e.preventDefault();
-    setIsLoginLoading(true);
-    setLoginError(null);
+  const loginUser = useCallback(
+    async (e) => {
+      e.preventDefault();
+      setIsLoginLoading(true);
+      setLoginError(null);
 
-    const response = await postRequest(
-      `${baseUrl}/users/login`,
-      JSON.stringify(loginInfo)
-    );
+      const response = await postRequest(
+        `${baseUrl}/users/login`,
+        JSON.stringify(loginInfo)
+      );
 
-    setIsLoginLoading(false);
+      setIsLoginLoading(false);
 
-    if (response.error) {
-      return setLoginError(response);
-    }
+      if (response.error) {
+        return setLoginError(response);
+      }
 
-    localStorage.setItem('User', JSON.stringify(response));
-    setUser(response);
-  }, [loginInfo])
+      localStorage.setItem("User", JSON.stringify(response));
+      setUser(response);
+    },
+    [loginInfo]
+  );
 
   const logoutUser = useCallback(() => {
-    localStorage.removeItem('User');
+    localStorage.removeItem("User");
     setUser(null);
-  }, [])
+  }, []);
 
   return (
     <AuthContext.Provider
@@ -96,7 +99,7 @@ export const AuthContextProvider = ({ children }) => {
         loginError,
         loginInfo,
         updateLoginInfo,
-        isLoginLoading
+        isLoginLoading,
       }}
     >
       {children}
--- client/src/context/ChatContext.jsx
+++ client/src/context/ChatContext.jsx
@@ -1,6 +1,6 @@
 /* eslint-disable react/prop-types */
-import { createContext, useEffect, useState } from "react";
-import { baseUrl, getRequest } from "../utils/services";
+import { createContext, useCallback, useEffect, useState } from "react";
+import { baseUrl, getRequest, postRequest } from "../utils/services";
 
 export const ChatContext = createContext();
 
@@ -8,27 +8,68 @@ export const ChatContextProvider = ({ children, user }) => {
   const [userChats, setUserChats] = useState(null);
   const [isUserChatsLoading, setIsUserChatsLoading] = useState(false);
   const [userChatsError, setUserChatsError] = useState(null);
+  const [potentialChats, setPotentialChats] = useState([]);
+
+  useEffect(() => {
+    const getUsers = async () => {
+      const response = await getRequest(`${baseUrl}/users`);
+
+      if (response.error) {
+        return console.log("Error fetching user", response);
+      }
+
+      const pChats = response.filter((u) => {
+        let isChatCreated = false;
+        if (user?._id === u._id) return false;
+
+        if (userChats) {
+          isChatCreated = userChats?.some((chat) => {
+            return chat.members[0] === u?._id || chat.members[1] === u?._id;
+          });
+        }
+
+        return !isChatCreated;
+      });
+
+      setPotentialChats(pChats);
+    };
+
+    getUsers();
+  }, [userChats]);
+
+  const createChat = useCallback(async (firstId, secondId) => {
+    const response = await postRequest(
+      `${baseUrl}/chats`,
+      JSON.stringify({ firstId, secondId })
+    );
+
+    if (response.error) {
+      return console.log("Error creating chat", response);
+    }
+
+    setUserChats((prev) => [...prev, response]);
+  }, []);
 
   useEffect(() => {
     const getUserChats = async () => {
-        if(user?._id) {
-            setIsUserChatsLoading(true);
-            setUserChatsError(null);
+      if (user?._id) {
+        setIsUserChatsLoading(true);
+        setUserChatsError(null);
 
-            const response = await getRequest(`${baseUrl}/chats/${user?._id}`);
+        const response = await getRequest(`${baseUrl}/chats/${user?._id}`);
 
-            setIsUserChatsLoading(false);
+        setIsUserChatsLoading(false);
 
-            if (response.error) {
-                return setUserChatsError(response);
-            }
-
-            setUserChats(response);
+        if (response.error) {
+          return setUserChatsError(response);
         }
-    }
+
+        setUserChats(response);
+      }
+    };
 
     getUserChats();
-  }, [user])
+  }, [user]);
 
   return (
     <ChatContext.Provider
@@ -36,6 +77,8 @@ export const ChatContextProvider = ({ children, user }) => {
         userChats,
         isUserChatsLoading,
         userChatsError,
+        potentialChats,
+        createChat,
       }}
     >
       {children}
