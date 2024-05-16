53f0da9fa0329ceb6f6a7eddc9db704f6cd89847
Paul Gomis <gomispapaul6@gmail.com>
2024-05-16 11:19:12 +0000

    fix cors error

--- socket/index.js
+++ socket/index.js
@@ -1,6 +1,6 @@
 const { Server } = require("socket.io");
 
-const io = new Server({ cors: "http://localhost:5173" });
+const io = new Server({ cors: ["http://localhost:5173", "https://msibiichat.vercel.app"] });
 
 let onlineUsers = [];
 
